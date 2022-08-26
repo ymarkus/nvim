cff2267f9b07b38ddbd8343abf420ebc77d4a5aa
Yannick Markus <ym@ymarkus.dev>
2022-08-26 12:32:29 +0200

    add twig parser

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -8,10 +8,20 @@ configs.setup({
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
+		-- disable = { "css" }, -- list of language that will be disabled
 	},
 	autopairs = {
 		enable = true,
 	},
 	indent = { enable = true, disable = { "python", "css" } },
 })
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.twig = {
+  install_info = {
+    url = "https://github.com/eirabben/tree-sitter-twig.git",
+    files = {"src/parser.c", "src/scanner.cc"},
+    branch = "main",
+  },
+  filetype = "twig",
+}
